ee192f651e947d04837ac18fc69250b255e859d4
mrr79 <marianarojas2197@gmail.com>
2023-03-19 17:43:51 -0600

    mover hacia arriba y hacia abajo

---
+++ prueba.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mrr on 19/03/23.
+//
+
+#include "prueba.h"
---
+++ prueba.h
@@ -0,0 +1,14 @@
+//
+// Created by mrr on 19/03/23.
+//
+
+#ifndef BATTLE_SPACE_PRUEBA_H
+#define BATTLE_SPACE_PRUEBA_H
+
+
+class prueba {
+
+};
+
+
+#endif //BATTLE_SPACE_PRUEBA_H
