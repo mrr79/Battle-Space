0e5966858f3e33b4320443c9f7268987a734fd15
mrr79 <marianarojas2197@gmail.com>
2023-03-21 23:51:27 -0600

    nave y disparo funcionando

--- nave_jugador.cpp
+++ nave_jugador.cpp
@@ -15,20 +15,19 @@ public:
     int x = 50;
     int y = 50;
     QImage image;
-    int bulletX = -1; // -1 means no bullet is currently active
+    int bulletX = -1; // -1 sigifica que no hay bala activa en este momento
     int bulletY = -1;
 
     MyShip(QWidget *parent = nullptr) : QWidget(parent) {
         // Load the image from file
         image.load("/home/mrr/Desktop/Battle-Space/myship.png");
 
-        // Create a timer that fires every second and triggers the creation of a bullet
+        // Create a timer that fires every second and triggers bullet
         QTimer *timer = new QTimer(this);
         connect(timer, &QTimer::timeout, this, &MyShip::createBullet);
-        timer->start(1000);
+        timer->start(500); // cada cuanto se dispara una bala
 
-        // Set the focus policy to accept keyboard events
-        setFocusPolicy(Qt::StrongFocus);
+        setFocusPolicy(Qt::StrongFocus); // set the focus policy: accept keyboard events
     }
 
     void paintEvent(QPaintEvent *event) override {
@@ -36,13 +35,15 @@ public:
         painter.drawImage(QRect(x, y, 50, 50), image);
 
         if (bulletX != -1 && bulletY != -1) {
-            // Draw the bullet if it's active
-            painter.fillRect(QRect(bulletX, bulletY, 10, 5), Qt::red);
+            // draw the bullet if is active
+            QImage bulletImage;
+            bulletImage.load("/home/mrr/Desktop/Battle-Space/bullet.png");
+            painter.drawImage(QRect(bulletX, bulletY, 20, 20), bulletImage);
         }
     }
 
     void keyPressEvent(QKeyEvent *event) override {
-        // Move the ship up or down depending on the arrow key pressed
+        // Move the ship ARRIBA Y ABAJO
         switch (event->key()) {
             case Qt::Key_Up:
                 y -= 10;
@@ -59,23 +60,23 @@ public:
 private:
     void createBullet() {
         if (bulletX == -1 && bulletY == -1) {
-            // Create a new bullet if there isn't one currently active
+            // Create un bullet si hay otro activo.
             bulletX = x + 50;
             bulletY = y + 25;
 
-            // Create a timer that fires every 20 microseconds and moves the bullet
+            //  timer fires every 20 useconds and moves the bullet
             QTimer *timer = new QTimer(this);
             connect(timer, &QTimer::timeout, this, &MyShip::moveBullet);
-            timer->start(20);
+            timer->start(20);//REALMENTE la velocidad a la que se mueve la bala
         }
     }
 
     void moveBullet() {
         if (bulletX > width()) {
-            // Deactivate the bullet when it goes off-screen
+            // Deactivate bullet when it goes off-screen
             bulletX = -1;
             bulletY = -1;
-            sender()->deleteLater(); // Delete the sender (the QTimer)
+            sender()->deleteLater(); // Delete the  QTimer)
         } else {
             bulletX += 10;
             update();
